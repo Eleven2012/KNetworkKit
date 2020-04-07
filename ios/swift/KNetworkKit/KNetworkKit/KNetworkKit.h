eff18670f9027ddaddca6a609d14e3f0fe318269
yulu.kong <yulu.kong@ubtrobot.com>
2020-04-07 14:58:25 +0800

    init project

---
+++ ios/swift/KNetworkKit/KNetworkKit/KNetworkKit.h
@@ -0,0 +1,19 @@
+//
+//  KNetworkKit.h
+//  KNetworkKit
+//
+//  Created by yulu kong on 2020/4/7.
+//  Copyright © 2020 yulu kong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KNetworkKit.
+FOUNDATION_EXPORT double KNetworkKitVersionNumber;
+
+//! Project version string for KNetworkKit.
+FOUNDATION_EXPORT const unsigned char KNetworkKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KNetworkKit/PublicHeader.h>
+
+
